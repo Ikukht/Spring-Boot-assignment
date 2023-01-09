682fd207a6689e5e44d245aab897f499b235e976
ikukht <ilyakukht@gmail.com>
2023-01-09 17:11:36 -0500

    Initial commit

---
+++ src/main/java/com/example/CapgeminiREST/CapgeminiRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.CapgeminiREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapgeminiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapgeminiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CapgeminiREST/CapgeminiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CapgeminiREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapgeminiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
